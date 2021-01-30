15e3c564ff8208be5f80b24955f66e183f4de097
Susan Tan <suetan93@gmail.com>
2021-01-29 19:27:52 -0800

    Procfile add

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path')
-const PORT = 3003
+const PORT = process.env.PORT || 3003
 
 const public = path.join(__dirname, '../client/dist')
 
