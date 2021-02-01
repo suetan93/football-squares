31136a02946ae57ca4da61b7766771dde9654c7f
Susan Tan <suetan93@gmail.com>
2021-02-01 09:10:27 -0800

    Remove console.log

--- client/src/components/Squares.jsx
+++ client/src/components/Squares.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const Square = ({ index, value, handleClick }) => {
-  console.log(value)
   return (
     <button id={`A${index}`} className="square" onClick={handleClick} ><b>{value}</b></button>
   )
